bc968967a717aac2ea1bdda89b4b80965e849a29
owlks4 <owlks4@users.noreply.github.com>
2024-01-23 19:25:16 +0000

    More updates

--- main.js
+++ main.js
@@ -1,52 +1,30 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 
-// The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
-
-  const width = 1920; // We will scale the photo width to this
-  let height = 0; // This will be computed based on the input stream
-
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
-
+  const width = 1920;
+  let height = 0;
   let streaming = false;
 
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
+  let video = document.getElementById("video");
+  let canvas = document.getElementById("canvas");
+  let startbutton = document.getElementById("startbutton");
+  let photosList = document.getElementById("photos-list");
+  let onionskins = document.getElementById("onionskins");
 
-  let video = null;
-  let canvas = null;
-  let photo = null;
-  let startbutton = null;
+  startbutton.onclick = (e) => {takepicture(); e.preventDefault();};
 
-  function startup() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error("Error: "+err);
+    });
 
-    video = document.getElementById("video");
-    canvas = document.getElementById("canvas");
-    photo = document.getElementById("photo");
-    startbutton = document.getElementById("startbutton");
-
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(`An error occurred: ${err}`);
-      });
-
-    video.addEventListener(
-      "canplay",
-      (ev) => {
-        if (!streaming) {
+  video.oncanplay = (e) => {
+      if (!streaming) {
           height = video.videoHeight / (video.videoWidth / width);
-
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
 
           if (isNaN(height)) {
             height = width / (4 / 3);
@@ -58,39 +36,7 @@ import viteLogo from '/vite.svg'
           canvas.setAttribute("height", height);
           streaming = true;
         }
-      },
-      false,
-    );
-
-    startbutton.addEventListener(
-      "click",
-      (ev) => {
-        takepicture();
-        ev.preventDefault();
-      },
-      false,
-    );
-
-    clearphoto();
-  }
-
-  // Fill the photo with an indication that none has been
-  // captured.
-
-  function clearphoto() {
-    const context = canvas.getContext("2d");
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    const data = canvas.toDataURL("image/png");
-    photo.setAttribute("src", data);
-  }
-
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
+      }
 
   function takepicture() {
     const context = canvas.getContext("2d");
@@ -100,12 +46,26 @@ import viteLogo from '/vite.svg'
       context.drawImage(video, 0, 0, width, height);
 
       const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
+
+      let newPhoto = document.createElement("img");
+      newPhoto.setAttribute("src", data);
+      newPhoto.setAttribute("class", "prospectivePhoto");
+      newPhoto.setAttribute("title", "Click to use as onionskin");
+      newPhoto.onclick = (e) => {addOnionSkin(e.target)};
+      photosList.appendChild(newPhoto);
     } else {
       clearphoto();
     }
   }
 
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  window.addEventListener("load", startup, false);
+  function addOnionSkin(originalImg){
+    let newOnionSkin = document.createElement("img");
+    newOnionSkin.id = originalImg.src; //intended.
+    newOnionSkin.src = originalImg.src; 
+    onionskins.appendChild(newOnionSkin);
+
+    let opacity = 1/(onionskins.childElementCount + 1);
+    for (const child of onionskins.children) { //updating all onionskins to have the new opacity so that they all remain at an equal slice of the whole
+      child.style = "height:100%;position:absolute;transform:translate(-50%,0%);opacity:"+opacity;
+    }
+  }
